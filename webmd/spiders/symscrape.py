ef9e9df83db5beb1231d8fbefec9f657f30d1876
matthewmazzuca <matthew.mazzuca@mail.utoronto.ca>
2016-01-17 01:35:14 -0500

    iterate through pages

--- webmd/spiders/symscrape.py
+++ webmd/spiders/symscrape.py
@@ -6,16 +6,34 @@ from webmd.items import WebmdItem
 
 class SymscrapeSpider(scrapy.Spider):
     name = "symscrape"
-    allowed_domains = ["http://www.webmd.com/a-to-z-guides/health-topics/default.htm"]
+    allowed_domains = ["www.webmd.com"]
     start_urls = (
         'http://www.webmd.com/a-to-z-guides/health-topics/default.htm',
     )
 
+    def parse(self,response):
+        site = response.selector.xpath('//div[@class="a-to-z alpha"]/ul/li')
+        urls = []
+        for href in site:
+            url = href.xpath('a/@href').extract()
+            url = "http://www.webmd.com" + url[0]
+            urls.append(url)
+        # print urls[0]
 
-    def parse(self, response):
+        for link in urls:
+            # print link
+            yield scrapy.Request(link, callback=self.parse_page)
+        # yield scrapy.Request(url, self.parse_page)
+
+
+
+
+    def parse_page(self, response):
 
     	sites = response.selector.xpath('//div[@class="a-to-z list"]/ul/li')
         items= []
+        # print "the link is: " + self
+        # print '\n'
 
     	for site in sites:
     		item = WebmdItem()
